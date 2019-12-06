9ebb3430f2b49716405ff56fa62f5626a6b860bd
Jason Lin <jason40418@gmail.com>
2019-12-07 01:05:49 +0800

    [Add] forge package to encrypt and use it

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,61 @@
 ﻿// 使用嚴格模式
 "use strict";
 
-(() => {
+/**
+ * @description 禁止表單自動轉頁送出，並且可客製化自己的判斷函數
+ * @param {*} object
+ * @param {*} action
+ *
+ * @version 1.0.0
+ * @author Jason Lin
+ *
+ * @requires
+ */
+var disable_submit_form = (object, action) => {
+  $('#' + object).validator().on('submit', (e) => {
+    // 表單送出未通過資料驗證
+    if (e.isDefaultPrevented()) {
+      // handle the invalid form...
+      console.log('error')
+    } else {
+      e.preventDefault();
+      action();
+    }
+    return false;
+  });
+}
 
-})()
+var current_time = () => {
+  Date.prototype.toIsoString = function () {
+    var tzo = -this.getTimezoneOffset(),
+      dif = tzo >= 0 ? '+' : '-',
+      pad = function (num) {
+        var norm = Math.floor(Math.abs(num));
+        return (norm < 10 ? '0' : '') + norm;
+      };
+    return this.getFullYear() +
+      '-' + pad(this.getMonth() + 1) +
+      '-' + pad(this.getDate()) +
+      'T' + pad(this.getHours()) +
+      ':' + pad(this.getMinutes()) +
+      ':' + pad(this.getSeconds()) +
+      dif + pad(tzo / 60) +
+      ':' + pad(tzo % 60);
+  }
+
+  let date = new Date();
+  let result = date.toIsoString().slice(0, 19).replace('T', ' ');
+
+  return result;
+}
+
+var alert = (title, message) => {
+  $.alert({
+    theme: 'modern',
+    icon: 'fa fa-minus-circle',
+    type: 'red',
+    animation: 'scale',
+    title: title,
+    content: message
+  });
+}
---
+++ static/js/key.js
@@ -0,0 +1,29 @@
+﻿// 使用嚴格模式
+"use strict";
+
+{
+
+  var rsa_encrypt = (data, object, public_key_id, key_id) => {
+    const data_string = JSON.stringify(data);
+    const key = $('#' + object);
+    const public_key = key.data(public_key_id);
+    const id = key.data(key_id);
+    // 公鑰物件
+    const public_key_obj = forge.pki.publicKeyFromPem(public_key);
+
+    // 前端使用者瀏覽器加密
+    let encrypted = public_key_obj.encrypt(data_string, "RSA-OAEP", {
+      md: forge.md.sha256.create(),
+      mgf1: forge.mgf1.create()
+    });
+
+    // 編碼成base64
+    let base64 = forge.util.encode64(encrypted);
+
+    return [id, base64];
+  }
+
+  //rsa_encrypt({ 'tai': 10 }, 'rsa-key', 'public-key', 'key-id');
+}
+
+
