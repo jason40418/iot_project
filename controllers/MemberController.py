08c290669fe6769cda751117415875372c9caef5
Jason Lin <jason40418@gmail.com>
2019-12-16 16:45:17 +0800

    [Add] Make template could read member profile

--- controllers/MemberController.py
+++ controllers/MemberController.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from flask import request, make_response, render_template
-from model.util.General import public_key_require_page, token_no_require_page
+from model.helper.MemberHelper import MemberHelper
+from model.util.General import public_key_require_page, token_no_require_page, token_require_page
 
 from __main__ import app
 
@@ -8,9 +9,20 @@ from __main__ import app
 member_blueprint = Blueprint('member', __name__)
 
 @member_blueprint.route("/", methods=['GET'])
-def index():
-    resp = make_response(render_template('app/member/index.html'))
-    return resp
+@token_require_page('/member/login')
+def index(payload):
+    # 取回會員資料
+    status, result, code = MemberHelper.get(payload['account'])
+    # 會員資料取回成功
+    if status:
+        print(result)
+        resp = make_response(render_template('app/member/index.html', member=result.get_all_parameter()))
+        return resp
+    # 會員資料取回失敗，重新導向登入頁面
+    else:
+        resp = make_response(redirect(url, code=302))
+        resp.set_cookie(key='token', value='', expires=0)
+        return resp
 
 @member_blueprint.route("/register", methods=['GET'])
 @token_no_require_page('/member')
